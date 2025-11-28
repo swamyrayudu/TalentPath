f9486d340c9f7aeba7544ef35f99e097a59fa190
Rayudu Veera Venkata Swamy <swamyrayudu7288@gmail.com>
2025-11-28 11:57:33 +0530

    deploy errors solve

--- src/components/contest/add-question-form.tsx
+++ src/components/contest/add-question-form.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
